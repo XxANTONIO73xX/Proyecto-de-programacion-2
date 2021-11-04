be002a05e33585361b54b02b9058aff6af548e2e
Pablo Ojeda <60475129+pablx33@users.noreply.github.com>
2021-11-04 15:53:43 -0700

    Test 1 P

---
+++ BaseBallApp/test/Interfaces/MostrarBuscarEliminar/EliminarJugadoresTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces.MostrarBuscarEliminar;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User
+ */
+public class EliminarJugadoresTest {
+    
+    public EliminarJugadoresTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of cbxEquipos method, of class EliminarJugadores.
+     */
+    @Test
+    public void testCbxEquipos() {
+        System.out.println("cbxEquipos");
+        EliminarJugadores instance = null;
+        instance.cbxEquipos();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of cargarTabla method, of class EliminarJugadores.
+     */
+    @Test
+    public void testCargarTabla() {
+        System.out.println("cargarTabla");
+        EliminarJugadores instance = null;
+        instance.cargarTabla();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of vaciarTabla method, of class EliminarJugadores.
+     */
+    @Test
+    public void testVaciarTabla() {
+        System.out.println("vaciarTabla");
+        EliminarJugadores instance = null;
+        instance.vaciarTabla();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
