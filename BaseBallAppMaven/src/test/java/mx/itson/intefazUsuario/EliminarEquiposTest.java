887c59d5d720d02a6e9c4292d5016183abf37f68
Pablo Ojeda <60475129+pablx33@users.noreply.github.com>
2021-11-05 15:57:29 -0700

    Interfaces Test

---
+++ BaseBallAppMaven/src/test/java/mx/itson/intefazUsuario/EliminarEquiposTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.intefazUsuario;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User
+ */
+public class EliminarEquiposTest {
+    
+    public EliminarEquiposTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of cargarTabla method, of class EliminarEquipos.
+     */
+    @Test
+    public void testCargarTabla() {
+        System.out.println("cargarTabla");
+        EliminarEquipos instance = null;
+        instance.cargarTabla();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of vaciarTabla method, of class EliminarEquipos.
+     */
+    @Test
+    public void testVaciarTabla() {
+        System.out.println("vaciarTabla");
+        EliminarEquipos instance = null;
+        instance.vaciarTabla();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
